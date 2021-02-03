31387f1532d4f15b38628de4ebceff898b771993
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-02-03 22:23:46 +0200

    create js less1 dir

---
+++ JS/lesson1/main.js
@@ -0,0 +1,463 @@
+//1-присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.8, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+
+//let greeting = 'hello';
+//console.log(greeting);
+//alert(greeting);
+//document.write(greeting );
+
+//let univer = 'owu';
+//console.log(univer);
+//alert(univer);
+//document.write(univer);
+
+//let domain = 'com';
+//console.log(domain);
+//alert(domain);
+//document.write(domain);
+
+//let int1 = 1;
+//console.log(int1);
+//alert(int1);
+//document.write(int1);
+
+//let int2 = 10;
+//console.log(int2);
+//alert(int2);
+//document.write(int2);
+
+//let int3 = -999;
+//console.log(int3);
+//alert(int3);
+//document.write(int3);
+
+//let int4 = 123;
+//console.log(int4);
+//alert(int4);
+//document.write(int4);
+
+// let pi = 3.14;
+//console.log(pi);
+//alert(pi);
+//document.write(pi)
+
+//let float = 2.8;
+//console.log(float);
+//alert(float);
+//document.write(float)
+
+//let int5 = 16;
+//console.log(int5);
+//alert(int5);
+//document.write(int5)
+
+///let boolean1 = true;
+//console.log(boolean1);
+//alert(boolean1);
+//document.write(boolean1)
+
+//let boolean2 = false;
+//console.log(boolean2);
+//alert(boolean2);
+//document.write(boolean2)
+
+//2- переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+
+
+//greeting = 'hi-ya!';
+//console.log(greeting);
+//alert(greeting);
+//document.write(greeting );
+//
+////*//
+//
+//univer = 'politeh';
+//console.log(univer);
+//alert(univer);
+//document.write(univer);
+
+//domain = 'net';
+//console.log(domain);
+//alert(domain);
+//document.write(domain);
+
+//int1 = 2;
+//console.log(int1);
+//alert(int1);
+//document.write(int1);
+
+//int2 = 100;
+//console.log(int2);
+//alert(int2);
+//document.write(int2);
+
+//int3 = -999;
+//console.log(int3);
+//alert(int3);
+//document.write(int3);
+
+//int4 = 321;
+//console.log(int4);
+//alert(int4);
+//document.write(int4);
+
+//pi = 3.16;
+//console.log(pi);
+//alert(pi);
+//document.write(pi)
+
+//float = 4.9;
+//console.log(float);
+//alert(float);
+//document.write(float)
+
+//int5 = 25;
+//console.log(int5);
+//alert(int5);
+//document.write(int5)
+
+//boolean1 = falce;
+//console.log(boolean1);
+//alert(boolean1);
+//document.write(boolean1)
+
+//boolean2 = true;
+//console.log(boolean2);
+//alert(boolean2);
+//document.write(boolean2)
+
+//3- Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+//const greeting = 'hi-ya!';
+//console.log(greeting);
+//alert(greeting);
+//document.write(greeting )
+
+//const univer = 'politeh';
+//console.log(univer);
+//alert(univer);
+//document.write(univer);
+
+//const domain = 'net';
+//console.log(domain);
+//alert(domain);
+//document.write(domain);
+
+//const int1 = 2;
+//console.log(int1);
+//alert(int1);
+//document.write(int1);
+
+//const int2 = 100;
+//console.log(int2);
+//alert(int2);
+//document.write(int2);
+
+//const int3 = -999;
+//console.log(int3);
+//alert(int3);
+//document.write(int3);
+
+//4- при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+
+// let userName = prompt('enter your name');
+// console.log(userName);
+// alert(userName);
+// document.write(userName);
+//
+// let userFathersName = prompt('enter your fathersname');
+// console.log(userFathersName);
+// alert(userFathersName);
+// document.write(userFathersName);
+//
+// let userSurname = prompt('enter your surname');
+// console.log(userSurname);
+// alert(userSurname);
+// document.write(userSurname);
+//
+// //5- Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+//
+// let person = (`${userName}  ${userFathersName}  ${userSurname}`);
+// console.log(person);
+// alert(person);
+// document.write(person);
+
+//6- Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+
+//let myWifeName = 'Olga';
+//let myWifeFathersName = 'Maryanivna';
+//let myWifeSurname = 'Krip';
+//let myWife = (myWifeName +' '+ myWifeFathersName +' '+ myWifeSurname);
+//console.log(myWife);
+//alert(myWife);
+//document.write(myWife) ;
+//
+//let mySonName = 'Zahar';
+//let mySonFathersName = 'Volodymyrovych';
+//let mySonSurname = 'Cherevko';
+//let mySon = (mySonName +' '+ mySonFathersName +' '+ mySonSurname);
+//console.log(mySon);
+//alert(mySon);
+//document.write(mySon) ;
+
+//8- при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль. 8
+
+// let a = +prompt('input a');
+// console.log(a);
+// let b = +prompt('input b');
+// console.log(b);
+// let c = +prompt('input c');
+// console.log(c);
+
+//9- при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+// let a = prompt('input a');
+// a = parseInt(a);
+// let b = prompt('input b');
+// b = parseInt(b);
+// let c = prompt('input c');
+// c = parseInt(c);
+// let d = prompt('input c');
+// d = parseInt(d);
+// let result = parseInt(a + b + c + d);
+// console.log(result);
+
+//9- при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+//let a = prompt('input a');
+//a = parseFloat(a);
+//let b = prompt('input b');
+//b = parseFloat(b);
+//let c = prompt('input c') ;
+//c = parseFloat(c);
+//let result = (a + b + c);
+//console.log(result)
+
+//10- при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+
+//let a = Math.round(parseFloat(prompt('input a')));
+//let b = Math.round(parseFloat(prompt('input a')));
+//let c = Math.round(parseFloat(prompt('input a')));
+//console.log(a + b + c);
+
+//11- при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу. Первое число - это число которое будут возводить в степень. Второе число - это число которое является степенью. При помощи Math.pow возвести первое число в степень второго числа.
+
+//let a = prompt('input a');
+//let b = prompt('input pow');
+//console.log(Math.pow(a , b));
+
+//12- При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+
+//let a = 100;
+//let b = '100';
+//let c = true;
+//let d = undefined;
+//console.log(typeof a);
+//console.log(typeof b);
+//console.log(typeof c);
+//console.log(typeof d);
+
+//13- Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
+// В одинаковых выражениях в задании не использовать два одинаковых оператора.
+// Каждое выражение вложить в свою переменную
+
+//let x = (5 < 6);
+//console.log(x);
+
+//let y = (5 > 6);
+//console.log(y);
+//
+//let z = (5 == 6);
+//console.log(z);
+//
+//let q = (5 === 6);
+//console.log(q);
+
+//let a = (10 == 10);
+//console.log(a);
+//
+//let b = (10 === 10);
+//console.log(b);
+//
+//let c = (10 < 10);
+//console.log(c);
+//
+//let d = (10 > 10);
+//console.log(d);
+//
+//let e = (10 != 10);
+//console.log(e);
+//
+//let f = (123 === '123');
+//console.log(f);
+//
+//let g = (123 == '123');
+//console.log(g);
+
+
+//Дополнительно:
+//- Посмотрев на следюующие выражения, скажите, каков будет вывод в консоль
+//(132 > 100 && 45 < 12 );======>false
+//(34 > 33 && 23 < 90 );======>true
+//(99 > 100 && 45 > 12 );======>false
+//(132 > 100 || 45 < 12 );======>true
+//(111 > 11 || 45 < 111 );======>true
+//((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) )======>true;
+//((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) )======>true;
+//((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ))======>false;
+//((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ))======>true;
+//(!!'-1');======>true
+//(!!-1);======>true
+//(!!'0');======>true
+//(!!'null');======>true
+//(!!'undefined');======>true
+//(!!(3/'owu'));======>false
+//((111 > 11 || 45 < 111) ||  !!'0');======>true
+//((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));======>false
+
+
+//CLASS
+
+//1.Определите переменные str, num, flag и txt со значениями «Привет», 123, true, «true».
+//   При помощи оператора typeof убедитесь, что значения переменных принадлежат типам: string, number и  boolean.
+
+//let str = 'Прювет';
+//let num = 123;
+//let flag = true;
+//let text = 'true';
+//console.log(typeof str);
+//console.log(typeof num);
+//console.log(typeof flag);
+//console.log(typeof text);
+
+//2. Создайте переменные a1, a2, a3, a4, a5. При помощи математических операторов (сложение, вычитание и т.д.) найдите значения выражений:
+
+//let a1 = 5 + 3;
+//let a2 = 5 - 3;
+//let a3 = 5 * 3;
+//let a4 = 5 / 3;
+//let a5 = 5 % 3;
+//console.log(`${a1} ${a2} ${a3} ${a4} ${a5} `)
+
+//3.  Создайте переменные a6, a7, a8, a9, a10. Поместите в них результат выражений:
+
+//let a6 = 5 % 3;
+//let a7 = 3 % 5;
+//let a8 = 5 + '3';
+//let a9 = '5' - 3;
+//let a10 = 75 + 'кг';
+//console.log(`${a6} ${a7} ${a8} ${a9} ${a10} `)
+
+//4. Напишите код, который находит площадь прямоугольника высота 23см. ( переменная height),
+//   шириной 10см (переменная width), значение площади должно хранится в числовой переменной s.
+//
+//let height = 23;
+//let width = 10;
+//let s = height * width;
+//console.log(s);
+//
+//5.  Напиши код, который находит объем цилиндра высотой 10м (переменная heightC) и диаметром основания 4м (dC),
+// результат поместите в переменную v.
+
+//let heightC = 10;
+//let dC = 4;
+//let r = dC/2;
+//const PI =3.14;
+//let v = (PI * Math.pow(r, 2) * heightC);
+//console.log (v);
+
+
+// =====================
+// ======ДОП============
+// =====================
+
+//1. Три різних числа вводяться через prompt().
+// За допомоги if else вивести іх в порядку зростання. (відсортувати по зростанню)
+
+// let a = +prompt('enter a');
+// let b = +prompt('enter b');
+// let c = +prompt('enter c');
+//
+// if (a > b && a > c) {
+//     if (b > c)
+//         console.log(c, b, a);
+//      else  {
+//         console.log(b, c, a);
+//     }
+// }
+//
+// else if (b > a && b > c) {
+//     if (c > a)
+//         console.log(a, c, b);
+//     else {
+//         console.log(c, a, b);
+//     }
+// }
+//
+// else if (c > a && c > b) {
+//     if (b > a)
+//         console.log(a, b, c);
+//     else
+//         console.log(b, a, c);
+// }
+
+
+//2.
+//Все параматры получаем с клавиатуры!!!!
+//Имитируем поведение пешехода на перекстке.
+//Если светофор зеленый - вывести "иди".
+//Если светофор желтый - вывести "подожди".
+//Если светофор красный - вывести "стой".
+//Если светофор в аварийном режиме вывести "делай что хочешь"!
+
+// let color = prompt('enter color')
+// if  (color === 'green'){
+//    console.log('go!!!')
+// }
+// else if  (color === 'orange') {
+//    console.log('wait')
+// }
+// else if (color === 'red') {
+//    console.log('stay!!!')
+// }
+//  else if  (color === 'blinking orange') {
+//      console.log('do what you wanna')
+// }
+
+
+//3
+//Все параметры получаем с клавиатуры!!!!(prompt , confirm)
+//Создать переменную isRoadClear которая характеризирует наличие на дороге машин.
+//Улучшаем предыдущее задание.
+//Если светофор зеленый и машин нет - вывести "иди".
+//Если светофор зеленый и машины есть  - вывести подожди пока нарушители проедут".
+//Если светофор желтый и машины есть - вывести "жди".
+//Если светофор желтый и машин нет - вывести "все рано жди".
+//Если светофор красный и машин нет- вывести "стой все рано".
+//Если светофор красный - и машины есть вывести "стой и жди".
+//Если светофор в аварийном режиме вывести "делай что хочешь"!
+
+// let color = prompt('enter color')
+// let isRoadClear = confirm()
+// if (color == 'green' && isRoadClear ) {
+//    console.log('go')
+// }
+// else if  (color == 'green' && !isRoadClear) {
+//    console.log('wait,offender')
+// }
+// else if  (color == 'orange' && !isRoadClear) {
+//    console.log('wait')
+// }
+// else if  (color == 'orange' && isRoadClear) {
+//    console.log('still wait')
+// }
+// else if  (color == 'red' && isRoadClear) {
+//    console.log('stay anyway')
+// }
+// else if  (color == 'red' && !isRoadClear) {
+//    console.log('stay & wait')
+// }
+// else if  (color == 'blinking orange') {
+//    console.log('do what you wanna')
+// }
+//
+
+
