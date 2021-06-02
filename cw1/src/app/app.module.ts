7b00b317e756555256ac27cd5f9ed731ba0a1157
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-06-02 11:28:50 +0300

    class work 1 done

--- cw1/src/app/app.module.ts
+++ cw1/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './components/posts/post/post.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { CommentComponent } from './components/comments/comment/comment.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostsComponent,
+    CommentComponent,
+    CommentsComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
