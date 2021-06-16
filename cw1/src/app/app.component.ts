567b52d92a04ae9a8f7a9105be29bc17a528e5df
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-06-16 13:41:14 +0300

    lesson 5

--- cw1/src/app/app.component.ts
+++ cw1/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {DataTransferService} from "./app-services/data-transfer.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,16 @@ import {DataTransferService} from "./app-services/data-transfer.service";
 })
 export class AppComponent {
 
-  constructor() { }
+
+  constructor(private httpClient: HttpClient) {
+
+    // this.httpClient.get('https://api.yelp.com/v3/businesses/search?location=NYC',
+    //   {
+    //     headers: new HttpHeaders(
+    //       {'Authorization': 'Bearer zs1c9sr7C6QjMzMs_S2yRnddQwl9G_T-88k-doOCkIQ0nJPnPIExUuvetUdr3AMUxkKlqsvNvPd5gG7rwsGY2AvuRVDfsDSsj57rwhKDBS8BoaGdSAhGr3dMWWXIYHYx'}
+    //     )
+    //   })
+    this.httpClient.get('https://jsonplaceholder.typicode.com/users')
+    .subscribe(value=>console.log(value))
   }
+}
--- cw1/src/app/app.module.ts
+++ cw1/src/app/app.module.ts
@@ -2,23 +2,27 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {RouterModule, Routes} from "@angular/router";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {CustomInterceptorService} from "./custom-interceptor.service";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
-  ],
+   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      {path:'users',loadChildren: ()=>import('./modules/user/user.module').then(m => m.UserModule)},
-      {path:'posts',loadChildren: ()=>import('./modules/post/post.module').then(m => m.PostModule)},
-      {path:'comments',loadChildren: ()=>import('./modules/comment/comment.module').then(m => m.CommentModule)},
-    ])
+    HttpClientModule,
+    FormsModule,
+   ],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:CustomInterceptorService,
+      multi:true
+    }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
